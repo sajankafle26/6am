60de226c51729398e6ed6665c3a91624b0ec54de
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2024-02-16 13:28:33 +0545

    Add files via upload

---
+++ magnificenttravels/js/main.js
@@ -0,0 +1,34 @@
+window.addEventListener("scroll", (event) => {
+   var a=scrollY;
+   if(a > 150)
+   {
+    document.querySelector('.navbar').classList.add('aaaaa')
+   }
+   else 
+   {
+    document.querySelector('.navbar').classList.remove('aaaaa')
+   }
+  });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
