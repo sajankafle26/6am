57ab4858cd250647d04c6a6045b33103dae4cc25
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2024-02-23 07:59:11 +0545

    Add files via upload

---
+++ three.js
@@ -0,0 +1,12 @@
+//Loop In JS for, while, do while, for in, for of , continue, break;
+
+for( let a=2; a< 10; a++)
+{
+    if(a==3)
+    {
+        console.log("Happy face 3");
+        //continue;
+        break;
+    }
+    console.log("for loop", a)
+}
